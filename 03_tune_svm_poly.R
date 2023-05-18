9434acf771993c2c2b23e3e61e02c00034987c5a
mleonardi6 <markjleonardi6@gmail.com>
2023-05-18 15:35:03 -0500

    pushed final scripts

--- 03_tune_svm_poly.R
+++ 03_tune_svm_poly.R
@@ -1,10 +1,10 @@
-### SVM Radial ----
+### SVM Poly ---
 
-# Load package(s) ----
 library(tidyverse)
 library(tidymodels)
 library(doMC)
 library(tictoc)
+
 
 # handle common conflicts
 tidymodels_prefer()
@@ -16,34 +16,36 @@ load("data/car_split.rda")
 load("data/kitchen_sink_recipe.rda")
 
 ## define model
-svm_radial_model <- svm_rbf(
-  mode = "classification", 
+svm_poly_model <- svm_poly(
+  mode = "classification",
   cost = tune(),
-  rbf_sigma = tune()
+  degree = tune(),
+  scale_factor = tune()
 ) %>%
   set_engine("kernlab")
 
 # set-up tuning grid ----
-svm_radial_params <- extract_parameter_set_dials(svm_radial_model)
+svm_poly_params <- extract_parameter_set_dials(svm_poly_model)
 
-svm_radial_grid <- grid_regular(svm_radial_params, levels = 5)
+svm_poly_grid <- grid_regular(svm_poly_params, levels = 5)
 
 # workflow ----
-svm_radial_workflow <- workflow() %>% 
-  add_model(svm_radial_model) %>% 
+svm_poly_workflow <- workflow() %>% 
+  add_model(svm_poly_model) %>% 
   add_recipe(kitchen_sink_recipe)
 
 # fitting
 tic.clearlog()
-tic("SVM Radial")
+tic("SVM Poly")
 
-svm_radial_tuned <- tune_grid(
-  svm_radial_workflow, 
-  resamples =  car_folds, 
-  grid = svm_radial_grid, 
+svm_poly_tuned <- tune_grid(
+  svm_poly_workflow, 
+  resamples = car_folds, 
+  grid = svm_poly_grid, 
   control = control_grid(save_pred = TRUE, 
                          save_workflow = TRUE, 
                          parallel_over = "everything"))
+
 
 # Pace tuning code in here
 toc(log = TRUE)
@@ -51,9 +53,9 @@ toc(log = TRUE)
 # save runtime info
 time_log <- tic.log(format = FALSE)
 
-svm_radial_tictoc <- tibble(model = time_log[[1]]$msg, 
-                            runtime = time_log[[1]]$toc - time_log[[1]]$tic)
+svm_poly_tictoc <- tibble(model = time_log[[1]]$msg, 
+                          runtime = time_log[[1]]$toc - time_log[[1]]$tic)
 
 # Write out results & workflow
 
-save(svm_radial_tuned, svm_radial_tictoc, file = "results/svm_radial_tuned.rda")
+save(svm_poly_tuned, svm_poly_tictoc, file = "results/svm_poly_tuned.rda")
