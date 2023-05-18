344d55fcc2086fb1da4fba424ae53d063070c6e1
mleonardi6 <markjleonardi6@gmail.com>
2023-05-18 15:24:39 -0500

    setup initial commit

---
+++ 2_setup.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(tidymodels)
+
+set.seed(111)
+
+### Load required objects
+load("data/car_split.rda")
+
+### Fold data
+
+car_folds <- car_train %>%  vfold_cv(v = 5, repeats = 3, strata = is_claim)
+
+### Save folds
+save(car_folds, file = "data/car_folds.rda")
+
+### Write kitchen sink recipe
+kitchen_sink_recipe <- recipe(is_claim ~ ., data = car_train) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_nzv(all_predictors()) %>%
+  step_normalize(all_predictors()) %>% 
+  step_corr(all_predictors())
+
+### Save recipe
+save(kitchen_sink_recipe, file = "data/kitchen_sink_recipe.rda")
+
+### Write another recipe
