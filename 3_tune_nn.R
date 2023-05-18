b1f596b3b17412562743e1a1ce99ebadccd67d7c
mleonardi6 <markjleonardi6@gmail.com>
2023-05-18 15:32:47 -0500

    committing scripts

---
+++ 3_tune_nn.R
@@ -0,0 +1,60 @@
+### Neural Network ----
+
+# Load package(s) ----
+library(tidyverse)
+library(tidymodels)
+library(doMC)
+library(tictoc)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load required objects ----
+registerDoMC(cores = 8)
+load("data/car_folds.rda")
+load("data/car_split.rda")
+load("data/kitchen_sink_recipe.rda")
+
+## define model
+nn_model <- mlp(
+  mode = "classification",
+  hidden_units = tune(),
+  penalty = tune()
+) %>%
+  set_engine("nnet")
+
+# set-up tuning grid ----
+nn_params <- extract_parameter_set_dials(nn_model)
+
+nn_grid <- grid_regular(nn_params, levels = 5)
+
+# workflow ----
+nn_workflow <- workflow() %>% 
+  add_model(nn_model) %>% 
+  add_recipe(kitchen_sink_recipe)
+
+# fitting
+tic.clearlog()
+tic("Neural Network")
+
+nn_tuned <- tune_grid(
+  nn_workflow, 
+  resamples = car_folds, 
+  grid = nn_grid, 
+  control = control_grid(save_pred = TRUE, 
+                         save_workflow = TRUE, 
+                         parallel_over = "everything"))
+
+
+# Pace tuning code in here
+toc(log = TRUE)
+
+# save runtime info
+time_log <- tic.log(format = FALSE)
+
+nn_tictoc <- tibble(model = time_log[[1]]$msg, 
+                    runtime = time_log[[1]]$toc - time_log[[1]]$tic)
+
+# Write out results & workflow
+
+save(nn_tuned, nn_tictoc, file = "results/nn_tuned.rda")
---
+++ 3_tune_svm_rad.R
@@ -0,0 +1,57 @@
+# Load package(s) ----
+library(tidyverse)
+library(tidymodels)
+library(doMC)
+library(tictoc)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load required objects ----
+registerDoMC(cores = 8)
+load("data/car_folds.rda")
+load("data/car_split.rda")
+load("data/kitchen_sink_recipe.rda")
+
+## define model
+svm_radial_model <- svm_rbf(
+  mode = "classification", 
+  cost = tune(),
+  rbf_sigma = tune()
+) %>%
+  set_engine("kernlab")
+
+# set-up tuning grid ----
+svm_radial_params <- extract_parameter_set_dials(svm_radial_model)
+
+svm_radial_grid <- grid_regular(svm_radial_params, levels = 5)
+
+# workflow ----
+svm_radial_workflow <- workflow() %>% 
+  add_model(svm_radial_model) %>% 
+  add_recipe(kitchen_sink_recipe)
+
+# fitting
+tic.clearlog()
+tic("SVM Radial")
+
+svm_radial_tuned <- tune_grid(
+  svm_radial_workflow, 
+  resamples =  car_folds, 
+  grid = svm_radial_grid, 
+  control = control_grid(save_pred = TRUE, 
+                         save_workflow = TRUE, 
+                         parallel_over = "everything"))
+
+# Pace tuning code in here
+toc(log = TRUE)
+
+# save runtime info
+time_log <- tic.log(format = FALSE)
+
+svm_radial_tictoc <- tibble(model = time_log[[1]]$msg, 
+                            runtime = time_log[[1]]$toc - time_log[[1]]$tic)
+
+# Write out results & workflow
+
+save(svm_radial_tuned, svm_radial_tictoc, file = "results/svm_radial_tuned.rda")
